a5a821ed10b72a2722f546c403c412fe767dc056
CiroBurro <filobaglioni.06@proton.me>
2025-10-16 18:39:13 +0200

    Main documented

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ import asyncio, time
 
 
 async def _scan_wrapper(host: str, port: int):
+    """
+    Wrapper to py_scan_port function, needed to allow python to interpret that function as async
+    :param host: ip address of the host to scan
+    :param port: port to scan
+    :return: a future with the scan result
+    """
     try:
         return await py_scan_port(host, port)
     except Exception as e:
         return e
 
 def print_ports(results, single_port):
-
+    """
+    Prints the result of the scan. It prints only the open and filtered ports if a range of ports was specified, instead if only a port was scanned it prints its status
+    :param results: list with the results of the scan
+    :param single_port: single_port flag specifies if only a single port was scanned
+    :return: Null
+    """
     for res in results:
         port = res.result()
 
@@ -22,6 +33,10 @@ def print_ports(results, single_port):
 
 
 async def main():
+    """
+    Main function of the program: collects the arguments, creates an async task to scan every port in parallel and finally prints the results
+    :return: Null
+    """
     start = time.time()
     single_port = False
 
